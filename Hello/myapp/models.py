5a4a7a50204bff3c1508cfb163aec3b382c52fd0
SwayamKohli <swayamkohli11489@gmail.com>
2025-01-01 23:56:05 +0530

    Update modes.py
    Created a Model for Government Schemes.

--- Hello/myapp/models.py
+++ Hello/myapp/models.py
@@ -3,13 +3,22 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     aadhaar_number = models.CharField(max_length=12, unique=True)
-    demographic_details = models.TextField()
+    demographic_details = models.TextField()  # You may want to structure this further
+
+    def __str__(self):
+        return self.name
 
 class GovernmentScheme(models.Model):
     name = models.CharField(max_length=200)
-    eligibility_criteria = models.TextField()
+    eligibility_criteria = models.JSONField()  # Changed to JSONField for structured data
     benefits = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 class UploadedDocument(models.Model):  # New model for uploaded documents
     document = models.FileField(upload_to='documents/')
     extracted_text = models.TextField(blank=True)  # Field to store extracted text
+
+    def __str__(self):
+        return self.document.name
